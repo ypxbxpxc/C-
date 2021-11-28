50b702cbf13f2195368484bc9a10678386e8aadd
Mr.Jopa <pavelrum1998@mail.ru>
2021-11-28 18:48:52 +0500

    dif

---
+++ dif/Program.cs
@@ -0,0 +1,7 @@
+﻿int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+double number1 = 12;
+double number2 = 5;
+double result = number1 / number2;
+Console.WriteLine(result);
