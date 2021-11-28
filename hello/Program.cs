4c794bdeba0fc3991fec3985bc3a874b3bd65b04
Mr.Jopa <pavelrum1998@mail.ru>
2021-11-28 19:07:48 +0500

    hello

---
+++ hello/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользоавтеля: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "jopa")
+{
+    Console.WriteLine("Ну здарова, тварына");
+}
+else
+{
+    Console.Write("Пшёл вон, ");
+    Console.Write(username);
+}
