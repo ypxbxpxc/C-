d84fa4180dea5026a08f83280ebb8333fbfa2e67
Mr.Jopa <pavelrum1998@mail.ru>
2021-11-28 20:13:24 +0500

    max

---
+++ max/Program.cs
@@ -0,0 +1,24 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 6, b1 = 10, c1 = 7, 
+a2 = 29, b2 = 15, c2 = 3,
+a3 = 28, b3 = 29, c3 = 32;
+
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+
+Console.Write("max = ");
+Console.WriteLine(max);
