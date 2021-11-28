eee8b672fcdcf1a9a9bf5900ae2a6929a35496e0
Mr.Jopa <pavelrum1998@mail.ru>
2021-11-28 20:32:57 +0500

    method

---
+++ method_array/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 5, 4, 19, 8, 15, 7, 4, 10};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index + 1;
+}
