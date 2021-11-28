2a75ca2f8acd2b52f01edb37bdd41f6c672b6f5e
Mr.Jopa <pavelrum1998@mail.ru>
2021-11-28 18:57:21 +0500

    RandomSum

--- sum/Program.cs
+++ sum/Program.cs
@@ -1,7 +1,14 @@
 ﻿int numberA = 3;
 int numberB = 5;
 Console.WriteLine(numberA + numberB);
+Console.WriteLine();
 int number1 = 25;
 int number2 = 31;
 int result = number1 + number2;
 Console.WriteLine(result);
+Console.WriteLine();
+int numberC = new Random().Next(1, 10);
+int numberD = new Random().Next(1, 10);
+Console.WriteLine(numberC);
+Console.WriteLine(numberD);
+Console.WriteLine(numberC + numberD);
