276f84326cbb8e215384e1c317faf702d44aa0f0
Mr.Jopa <pavelrum1998@mail.ru>
2021-11-28 23:18:45 +0500

    lection3

---
+++ for/Program.cs
@@ -0,0 +1,13 @@
+﻿string Method4(int count, string c)
+{
+    
+    string result = String.Empty;
+    for (int i = 1; i < count; i++)
+
+    {
+        result = result + c;
+    }
+    return result;
+}
+string res = Method4(10, "jopa");
+Console.WriteLine(res);
